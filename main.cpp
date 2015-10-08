4dbdd7f01de5782a6f122c6236db49da00d6fa11
Thomio Watanabe <thomio.watanabe@gmail.com>
2015-10-08 16:03:22 -0300

    Move main window to the screen center

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include "widget.h"
 #include <QApplication>
-
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication crop_image(argc, argv);
 
-    Widget w;
-    w.setCropPercentage("0.2");
-    w.show();
+    Widget window;
+
+    // Set default crop percentage
+    window.setCropPercentage("0.2");
+
+    // Move main window to sceen center
+    QRect screenGeometry = QApplication::desktop()->screenGeometry();
+    int x = screenGeometry.width() / 2;
+    int y = screenGeometry.height() / 2;
+    window.move(x,y);
+
+    window.show();
 	
     return crop_image.exec();
 }
